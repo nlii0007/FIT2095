41ddaa41c5f067dd68eb9e8f33d8dc8e57129114
Na Li <nali@dyn-49-127-146-216.its.monash.edu.au>
2019-08-27 09:10:27 +1000

    Initial Commit

---
+++ week5.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+
+let db = [];
+
+let viewsPath = __dirname + "/views";
+
+
+app.use(express.static("image"));
+
+app.engine("html",require('ejs').renderFile);
+app.set("view engine","html");
+
+app.use(bodyParser.urlencoded({
+    extended:false
+}));//values can be only string or array
+
+//Homepage
+app.get("/", function(req,res){
+    let filename = viewsPath + "/index.html"
+    res.sendFile(filename);
+});
+
+
+//add new tasks
+app.get("/addNewTask", function(req,res){
+    let filename = viewsPath + "/addTask.html";
+    res.sendFile(filename);
+})
+
+
+//list all tasks
+app.get("/getAllTasks", function(req,res){
+    let filename = viewsPath + "/allTasks.html";
+    res.render(filename,{Task:db});
+})
+
+
+app.post("/addmyTasks", function(req,res){
+    console.log(req.body);
+
+    db.push(req.body);
+    res.redirect("/getAllTasks")
+});
+
+app.listen(8080);
